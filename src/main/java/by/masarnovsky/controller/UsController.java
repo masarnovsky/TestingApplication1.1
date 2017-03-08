901add48fb46adb5d683df342c896a5ce3303bd1
Max Masarnovsky <m.masarnovsky@gmail.com>
2017-03-08 18:41:04 +0300

    trash data deleted

--- src/main/java/by/masarnovsky/controller/UsController.java
+++ src/main/java/by/masarnovsky/controller/UsController.java
@@ -1,5 +1,7 @@
 package by.masarnovsky.controller;
 
+import by.masarnovsky.DatabaseConnection;
+import by.masarnovsky.model.Answer;
 import by.masarnovsky.model.Question;
 import by.masarnovsky.service.AnswerService;
 import by.masarnovsky.service.QuestionService;
@@ -9,7 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
 
 
 @Controller
@@ -38,6 +44,108 @@ public class UsController {
        ui.addAttribute("quest", q);
        ui.addAttribute("answ", answerService.getAnswersForQuestion(q.get(1)));
         return "endTesting";
+    }
+
+    private List<Question> questions;
+    private List<Answer> answers;
+    private Map<Integer, List<Answer>> answersMap;
+    private int qId = 0;
+    private int rughtAnsw = 0;
+
+
+
+
+    @RequestMapping("/getNextQuestion")
+    String getNextQuestion(Model ui){
+        String page = "test";
+        if (qId >= questions.size()){
+            rughtAnsw = 1;
+            page = showEnd(ui);
+        } else {
+            ui.addAttribute("answersMap", answersMap);
+            ui.addAttribute("questions", questions);
+            ui.addAttribute("qId", qId);
+            qId++;
+        }
+
+        return page;
+    }
+
+    String showEnd(Model ui){
+        ui.addAttribute("rightAnsw", rughtAnsw);
+        ui.addAttribute("avg", questions.size()/2);
+        return "endTesting";
+    }
+
+    //@RequestMapping("/startTest")
+    @RequestMapping("/")
+    String pageTest(){
+        return "startTest";
+    }
+
+    @RequestMapping("/startTestNow")
+    String setTests(Model ui){
+        qId = 0;
+        rughtAnsw = 0;
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        final String GET_ALL_QUESTIONS = "select * from questions";
+        final String GET_ALL_ANSQWERS = "select * from answers";
+
+        try {
+            connection = DatabaseConnection.getConnection();
+            statement = connection.createStatement();
+            rs = statement.executeQuery(GET_ALL_QUESTIONS);
+            questions = new ArrayList<Question>();
+            while (rs.next()){
+                //System.out.println("+");
+                questions.add(new Question(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getInt(5)));
+            }
+            //System.out.println(questions);
+
+            answersMap = new HashMap<Integer, List<Answer>>();
+            rs = statement.executeQuery(GET_ALL_ANSQWERS);
+            while (rs.next()){
+                Answer a = new Answer(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getBoolean(4));
+                if (answersMap.get(new Integer(a.getQuestionId())) == null){
+                    List<Answer> aList = new ArrayList<Answer>();
+                    aList.add(a);
+                    answersMap.put(a.getQuestionId(), aList);
+                }
+                else {
+                    List<Answer> answerList = answersMap.get(new Integer(a.getQuestionId()));
+                    answerList.add(a);
+                }
+            }
+            Collections.shuffle(questions);
+
+            ui.addAttribute("answersMap", answersMap);
+            ui.addAttribute("questions", questions);
+            ui.addAttribute("qId", qId);
+            qId++;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null){
+                    rs.close();
+                }
+                if (statement != null){
+                    statement.close();
+                }
+                if(connection != null)
+                {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return "test";
     }
 
 }
