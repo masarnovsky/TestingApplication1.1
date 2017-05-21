c44375dd65c99aada5cd50c9ff38b7dd4c642c6c
Max Masarnovsky <m.masarnovsky@gmail.com>
2017-05-21 23:32:49 +0300

    fix js script for output variants

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -58,35 +58,88 @@ function historyAction() {
 
 var activeType;
 
-var firstType = '<div id="firstType">' +
-    '<div class="col s12">' +
-    '<div class="input-field col s12">' +
-    '<label>Правильный ответ:</label>' +
-    '<input name="answer1" type="text" />'+
-    '</div><div class="input-field col s12">'+
-    '<label>Ответ:</label><input name="answer2" type="text" /></div>'+
-    '<div class="input-field col s12"><label>Ответ:</label>'+
-    '<input name="answer3" type="text" /></div><div class="input-field col s12">' +
-    '<label>Ответ:</label><input name="answer4" type="text" /></div></div></div>';
+var firstType =
+    '<div id="firstType">' +
+        '<div class="col s12">' +
+            '<div class="input-field col s12">' +
+                '<label>Правильный ответ:</label>' +
+                '<input name="answer1" type="text" />'+
+            '</div>' +
+            '<div class="input-field col s12">'+
+                    '<label>Ответ:</label>' +
+                    '<input name="answer2" type="text" />' +
+            '</div>'+
+            '<div class="input-field col s12">' +
+                '<label>Ответ:</label>'+
+                '<input name="answer3" type="text" />' +
+            '</div>' +
+            '<div class="input-field col s12">' +
+                '<label>Ответ:</label>' +
+                '<input name="answer4" type="text" />' +
+            '</div>' +
+        '</div>' +
+    '</div>';
 
-var secondType = '<div id="secondType"><div class="col s12"><div class="col s12"><div class="input-field col s12">'  +
-    '<label>Правильный ответ:</label><input name="answer1" type="text" /></div><div class="input-field col s12">' +
-'<label>Ответ:</label><input name="answer2" type="text" /></div></div></div></div>';
+var secondType =
+    '<div id="secondType">' +
+        '<div class="col s12">' +
+            '<div class="input-field col s12">'  +
+                '<label>Правильный ответ:</label>' +
+                '<input name="answer1" type="text" />' +
+            '</div>' +
+            '<div class="input-field col s12">' +
+                '<label>Ответ:</label>' +
+                '<input name="answer2" type="text" />' +
+            '</div>' +
+        '</div>' +
+    '</div>';
 
-var thirdType = '<div id="thirdType"><div class="col s12">'+
-    '<div class="col s12 file-field input-field"><div class="btn"><span>Загрузить картинку</span>'+
-    '<input type="file" name="image" accept="image/*"></div><div class="file-path-wrapper">'+
-    '<input name="image-name" class="file-path validate" type="text"></div></div></div><div class="col s12">'+
-    '<div class="col s12"><div class="input-field col s12"><label>Правильный ответ:</label>'+
-    '<input name="answer1" type="text" /></div><div class="input-field col s12"><label>Ответ:</label>'+
-    '<input name="answer2" type="text" /></div><div class="input-field col s12">'+
-    '<label>Ответ:</label><input name="answer3" type="text" /></div><div class="input-field col s12">'+
-    '<label>Ответ:</label><input name="answer4" type="text" /></div></div></div></div>';
+var thirdType =
+    '<div id="thirdType">' +
+        '<div class="col s12">'+
+            '<div class="col s12 file-field input-field">' +
+                '<div class="btn"><span>Загрузить картинку</span>'+
+                    '<input type="file" name="image" accept="image/*">' +
+                '</div>' +
+                '<div class="file-path-wrapper">'+
+                    '<input name="image-name" class="file-path validate" type="text">' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
 
-var fourthType = '<div id="fourthType"><div class="col s12"><div class="col s12 file-field input-field">' +
-    '<div class="btn"><span>Загрузить картинку</span><input type="file" name="image" accept="image/*">' +
-    '</div><div class="file-path-wrapper"><input class="file-path validate" type="text">' +
-    '</div></div></div></div>';
+        '<div class="col s12">'+
+            '<div class="input-field col s12">' +
+                '<label>Правильный ответ:</label>'+
+                '<input name="answer1" type="text" />' +
+            '</div>' +
+            '<div class="input-field col s12">' +
+                '<label>Ответ:</label>'+
+                '<input name="answer2" type="text" />' +
+            '</div>' +
+            '<div class="input-field col s12">'+
+                '<label>Ответ:</label>' +
+                '<input name="answer3" type="text" />' +
+            '</div>' +
+            '<div class="input-field col s12">'+
+                '<label>Ответ:</label>' +
+                '<input name="answer4" type="text" />' +
+            '</div>' +
+        '</div>' +
+    '</div>';
+
+var fourthType =
+    '<div id="fourthType">' +
+        '<div class="col s12">' +
+            '<div class="col s12 file-field input-field">' +
+                '<div class="btn"><span>Загрузить картинку</span>' +
+                    '<input type="file" name="image" accept="image/*">' +
+                '</div>' +
+                '<div class="file-path-wrapper">' +
+                    '<input class="file-path validate" type="text">' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>';
 
 function qTypeChanged(v){
     if (v == 1){
